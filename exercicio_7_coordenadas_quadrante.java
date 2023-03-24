478f8bf80284559d0dd558afaf4db39b35859846
Aliane E. Amaral <108065159+AlianeAmaral@users.noreply.github.com>
2023-03-24 00:09:24 -0400

    Update exercicio_7_coordenadas_quadrante.java

--- exercicio_7_coordenadas_quadrante.java
+++ exercicio_7_coordenadas_quadrante.java
@@ -1,7 +1,7 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-public class exercicio_7_coordenadas_quadrante {
+public class exercicio_7_coordenadas_quadrante { 
 
 	public static void main(String[] args) {
 
